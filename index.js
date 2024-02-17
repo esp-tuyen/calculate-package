5561015a2fd7f5d304c976ad68acce08bc1fe5ea
tuyen <tuyen.tran@eastplayers.io>
2024-02-17 14:11:13 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+export const sum = (numbers) =>
+  numbers.reduce((total, number) => total + number, 0);
+
+export const plus = (a, b) => a + b;
+
+export const minus = (a, b) => a - b;
+
+export const multiplication = (a, b) => a * b;
+
+export const division = (a, b) => a / b;
