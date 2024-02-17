0a7cd32260392b33e2ec2bcb641d94baa4a61afb
tuyen <tuyen.tran@eastplayers.io>
2024-02-17 14:25:29 +0700

    upgrade version

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-export const sum = (numbers) =>
-  numbers.reduce((total, number) => total + number, 0);
+const sum = (numbers) => numbers.reduce((total, number) => total + number, 0);
 
-export const plus = (a, b) => a + b;
+const plus = (a, b) => a + b;
 
-export const minus = (a, b) => a - b;
+const minus = (a, b) => a - b;
 
-export const multiplication = (a, b) => a * b;
+const multiplication = (a, b) => a * b;
 
-export const division = (a, b) => a / b;
+const division = (a, b) => a / b;
+
+export default { sum, plus, minus, multiplication, division };
