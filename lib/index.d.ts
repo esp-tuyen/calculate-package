02efe66406990239676b6e98c396aa39fec30e7a
tuyen <tuyen.tran@eastplayers.io>
2024-02-17 16:03:11 +0700

    update: export

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,7 +1,14 @@
 export type SumProps = number;
-declare const sum: (numbers: SumProps[]) => number;
-declare const plus: (a: number, b: number) => number;
-declare const minus: (a: number, b: number) => number;
-declare const multiplication: (a: number, b: number) => number;
-declare const division: (a: number, b: number) => number;
-export { division, minus, multiplication, plus, sum };
+export declare const sum: (numbers: SumProps[]) => number;
+export declare const plus: (a: number, b: number) => number;
+export declare const minus: (a: number, b: number) => number;
+export declare const multiplication: (a: number, b: number) => number;
+export declare const division: (a: number, b: number) => number;
+declare const calc: {
+    division: (a: number, b: number) => number;
+    minus: (a: number, b: number) => number;
+    multiplication: (a: number, b: number) => number;
+    plus: (a: number, b: number) => number;
+    sum: (numbers: SumProps[]) => number;
+};
+export default calc;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 export type SumProps = number;
 
-const sum = (numbers: SumProps[]) =>
+export const sum = (numbers: SumProps[]) =>
   numbers.reduce((total, number) => total + number, 0);
 
-const plus = (a: number, b: number) => a + b;
+export const plus = (a: number, b: number) => a + b;
 
-const minus = (a: number, b: number) => a - b;
+export const minus = (a: number, b: number) => a - b;
 
-const multiplication = (a: number, b: number) => a * b;
+export const multiplication = (a: number, b: number) => a * b;
 
-const division = (a: number, b: number) => a / b;
+export const division = (a: number, b: number) => a / b;
 
-export { division, minus, multiplication, plus, sum };
+const calc = { division, minus, multiplication, plus, sum };
+
+export default calc;
