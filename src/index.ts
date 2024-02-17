06e1161b89be41e951acbf4c8783713cc169e3c5
tuyen <tuyen.tran@eastplayers.io>
2024-02-17 14:52:00 +0700

    update: add typescript for package

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export type SumProps = number;
+
+const sum = (numbers: SumProps[]) =>
+  numbers.reduce((total, number) => total + number, 0);
+
+const plus = (a: number, b: number) => a + b;
+
+const minus = (a: number, b: number) => a - b;
+
+const multiplication = (a: number, b: number) => a * b;
+
+const division = (a: number, b: number) => a / b;
+
+export default { sum, plus, minus, multiplication, division };
