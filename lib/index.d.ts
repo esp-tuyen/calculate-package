0a201b31667d3cc91cf6300cc03b275fcd42ad11
tuyen <tuyen.tran@eastplayers.io>
2024-02-17 14:58:34 +0700

    update: typescript

---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+export type SumProps = number;
+declare const _default: {
+    sum: (numbers: number[]) => number;
+    plus: (a: number, b: number) => number;
+    minus: (a: number, b: number) => number;
+    multiplication: (a: number, b: number) => number;
+    division: (a: number, b: number) => number;
+};
+export default _default;
